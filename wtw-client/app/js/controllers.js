471f37fdf1324f301dff5840e03227e29aea1dd4
Vince Wong <vincibleweb@gmail.com>
2014-03-14 08:51:21 +0000

    wtw-client further responsive design

--- wtw-client/app/js/controllers.js
+++ wtw-client/app/js/controllers.js
@@ -11,6 +11,40 @@
   function ReportCtrl($scope, $http, $filter) {
     var queryReceived = false;
     
+    // Change number of columns according to screen width
+    function setupMediaQueries() {
+      if (window.matchMedia) {
+        var smallMq = window.matchMedia("(max-width: 767px)");
+        var medMq = window.matchMedia("(min-width: 768px) and (max-width: 1199px)");
+        var largeMq = window.matchMedia("(min-width: 1200px)");
+        
+        var onMediaWidthChange = function (mq) {
+          if (smallMq.matches) {
+            $scope.forecastMaxCount = 5;
+          } else if (medMq.matches) {
+            $scope.forecastMaxCount = 7;
+          }
+          else if (largeMq.matches) {
+            $scope.forecastMaxCount = 9;
+          }
+          
+          // If responding to width change after startup, when $digest is not already in progress
+          if (mq) {
+            $scope.$apply();
+          }
+        };
+        
+        smallMq.addListener(onMediaWidthChange);
+        medMq.addListener(onMediaWidthChange);
+        largeMq.addListener(onMediaWidthChange);
+        
+        // Call immediately at startup to set correct forecastMaxCount
+        onMediaWidthChange();
+      } else {
+        // TODO: Fallback if matchMedia() not supported
+      }
+    }
+    
     //Return array of days. Each day has a name, and number of forecasts that day.
     function getForecastDays() {
       var days = [];
@@ -55,7 +89,7 @@
       return aheadIndices;
     };
     
-    function onQuerySuccess(data) {
+    function onForecastQuerySuccess(data) {
       $scope.availableForecastFreq = data.forecastFreq;
       $scope.forecastTimes = data.forecastTimes;
       $scope.aheadTimes = data.aheadTimes;
@@ -68,7 +102,7 @@
     }
     
     // Query weather data for +/- maxDays from now
-    function doQuery() {
+    function doForecastQuery() {
       var maxDays = 5;
       
       var start = new Date();
@@ -80,7 +114,7 @@
       var query = 'http://www.vincibleweb.com:3000/wtw/data/json/forecasts?serviceId=1&locationId=1&start=' + start.toJSON() + '&end=' + end.toJSON();
       
       var promise = $http.get(query);    
-      promise.success(onQuerySuccess);
+      promise.success(onForecastQuerySuccess);
     };
     
     $scope.onEarlier = function() {
@@ -98,6 +132,14 @@
       }
     };
     
+    $scope.onLess = function () {
+      $scope.forecastMaxCount--;
+    };
+    
+    $scope.onMore = function () {
+      $scope.forecastMaxCount++;
+    };
+    
     $scope.getCellStyle = function(cell) {
       var translation = 'translate3d(' + (cell.col * 5) + 'rem, ' + (cell.row * 5) + 'rem, 0rem)';
       var transform = {
@@ -112,20 +154,42 @@
     };
     
     $scope.getLayoutStyle = function(cell) {
-      var transform = {};
       
-      transform.left = ((cell.col / $scope.forecastMaxCount) * 100) + '%';
-      transform.top = ((cell.row / $scope.aheadMaxCount ) * 100) + '%';
+      // Day and time rows half height
+      function getEffeciveRow(realRow) {
+        var effectiveRow = 0;
+        
+        switch (realRow) {
+        case 0:
+          break;
+          
+        case 1:
+          effectiveRow = 0.5;
+          break;
+          
+        default:
+          effectiveRow = realRow - 1;
+        }
+        
+        return effectiveRow;
+      }
+      
+      var transform = {};
+      var colCount = $scope.forecastMaxCount + 1; // For ahead column
+      var rowCount = $scope.aheadMaxCount + 1; // Day and time rows half height
+      
+      transform.left = ((cell.col / colCount) * 100) + '%';
+      transform.top = ((getEffeciveRow(cell.row) / rowCount) * 100) + '%';
 
       if (cell.colspan) {
-        transform.width = ((cell.colspan / $scope.forecastMaxCount) * 100) + '%';
+        transform.width = ((cell.colspan / colCount) * 100) + '%';
       }
       else
       {
-        transform.width = ((1 / $scope.forecastMaxCount) * 100) + '%';
+        transform.width = ((1 / colCount) * 100) + '%';
       }
       
-      transform.height = ((1 / $scope.aheadMaxCount) * 100) + '%';
+      transform.height = ((1 / rowCount) * 100) + '%';
         
       return transform;
     };
@@ -148,6 +212,11 @@
       $scope.aheadIndices = getAheadIndices();
     });
     
+    $scope.$watch('forecastMaxCount', function() {
+      $scope.forecastIndices = getForecastIndices();
+      $scope.forecastDays = getForecastDays();
+    });
+    
     $scope.showTemps = true;
     
     $scope.forecastFreqs = [1, 3, 6, 12, 24];
@@ -161,6 +230,7 @@
     $scope.aheadFreq = 3;
     $scope.aheadIndices = [];
     
-    doQuery();
+    setupMediaQueries();
+    doForecastQuery();
   };
 }());
